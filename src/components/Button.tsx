a71771ac45c6e92865ba7e98ec9251f82db91fc6
zilkupcin <zil.kupcin@gmail.com>
2022-02-06 22:33:55 +0000

    Create a Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,24 +1,37 @@
 import { FC } from "react";
 import styled from "styled-components";
 
-const StyledButton = styled.button`
+const StyledButton = styled.input`
   min-width: 200px;
+  width: 100%;
   text-align: center;
   color: #fff;
   font-weight: 700;
   border-radius: 24px;
-  padding: 12px;
-  margin-top: 24px;
+  padding: 16px 24px;
+  margin-top: 32px;
   background-color: ${(props) => props.theme.colors.primaryDark};
+  cursor: pointer;
+  transition: opacity 0.2s ease-in-out;
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  @media only screen and (min-width: 480px) {
+    width: 200px;
+  }
 `;
 
 interface Props {
-  onClick: () => void;
+  onClick?: () => void;
   title: string;
 }
 
 const Button: FC<Props> = ({ onClick, title }) => {
-  return <StyledButton onClick={onClick}>{title}</StyledButton>;
+  return (
+    <StyledButton type="submit" onClick={onClick} value={title}></StyledButton>
+  );
 };
 
 export default Button;
