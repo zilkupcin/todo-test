b84ffe6618ecdee40374bcd836cd901ead6500a3
zilkupcin <zil.kupcin@gmail.com>
2022-02-06 16:57:06 +0000

    Create a Home page

---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,14 @@
+import { FC, useEffect } from "react";
+import Content from "../Content";
+import Footer from "../Footer";
+
+const Home: FC = () => {
+  return (
+    <>
+      <Content />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
