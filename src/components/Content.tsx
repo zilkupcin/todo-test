a3882119f187315c4cde02140847e759232219dc
zilkupcin <zil.kupcin@gmail.com>
2022-02-06 22:33:42 +0000

    Implement modal logic

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,5 +1,9 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import { RootStateOrAny, useDispatch, useSelector } from "react-redux";
+import { bindActionCreators } from "redux";
 import styled from "styled-components";
+import { actionCreators } from "../state";
+import Modal from "./Modal";
 import TodoList from "./TodoList";
 
 const Container = styled.main`
@@ -16,16 +20,77 @@ const Container = styled.main`
 `;
 
 const StyledTodoCounter = styled.h2`
-  margin-bottom: 18px;
+  margin-bottom: 32px;
   margin-top: 6px;
   font-size: 0.875rem;
+  font-weight: 600;
+  text-align: center;
+  color: ${(props) => props.theme.colors.greyThree};
 `;
 
 const Content: FC = () => {
+  const [isModalShown, setIsModalShown] = useState(false);
+  const todos = useSelector((state: RootStateOrAny) => state.todos);
+  const isTargetAchieved = useSelector(
+    (state: RootStateOrAny) => state.isTargetAchieved
+  );
+  const dispatch = useDispatch();
+
+  const { removeTodo, completeTodo, setTargetAchieved } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
+
+  const handleRemoveTodo = (todo: Todo) => {
+    removeTodo(todo);
+  };
+
+  const handleCompleteTodo = (todo: Todo) => {
+    completeTodo(todo);
+  };
+
+  const getIncompleteTaskCount = () => {
+    return todos.filter((todo: Todo) => !todo.completedDate).length;
+  };
+
+  useEffect(() => {
+    if (isTargetAchieved) return;
+
+    const tasksCompletedToday = todos.filter((todo: Todo) => {
+      const today = new Date();
+
+      return (
+        today.getDate() === todo.completedDate?.getDate() &&
+        today.getMonth() === todo.completedDate?.getMonth() &&
+        today.getFullYear() === todo.completedDate?.getFullYear()
+      );
+    }).length;
+
+    if (tasksCompletedToday === 3) {
+      setIsModalShown(true);
+      setTargetAchieved();
+    }
+  }, [todos]);
+
+  const handleModalClose = () => {
+    setIsModalShown(false);
+  };
+
+  const incompleteTaskCount = getIncompleteTaskCount();
+
   return (
     <Container>
-      <StyledTodoCounter>Your have 2 tasks to complete!</StyledTodoCounter>
-      <TodoList />
+      <StyledTodoCounter>
+        {incompleteTaskCount === 0
+          ? "You have no tasks to complete!"
+          : `Your have ${incompleteTaskCount} tasks to complete`}
+      </StyledTodoCounter>
+      <TodoList
+        todos={todos}
+        onRemoveClick={handleRemoveTodo}
+        onCompleteClick={handleCompleteTodo}
+      />
+      {isModalShown && <Modal onClose={handleModalClose} />}
     </Container>
   );
 };
