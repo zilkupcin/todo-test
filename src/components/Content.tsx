147782aa0c121454641bd4ee4a828d5c60ea5706
zilkupcin <zil.kupcin@gmail.com>
2022-02-06 15:08:22 +0000

    Create a Content component

---
+++ src/components/Content.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+import styled from "styled-components";
+import TodoList from "./TodoList";
+
+const Container = styled.main`
+  flex-grow: 1;
+  padding: 12px;
+
+  @media only screen and (min-width: 480px) {
+    padding: 24px;
+  }
+`;
+
+const StyledTodoCounter = styled.h2`
+  margin-bottom: 18px;
+  margin-top: 6px;
+  font-size: 0.875rem;
+`;
+
+const Content: FC = () => {
+  return (
+    <Container>
+      <StyledTodoCounter>Your have 2 tasks to complete!</StyledTodoCounter>
+      <TodoList />
+    </Container>
+  );
+};
+
+export default Content;
