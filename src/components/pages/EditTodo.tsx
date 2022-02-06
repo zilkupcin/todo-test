f0703cf829a57785760c2021f47f42c24a737701
zilkupcin <zil.kupcin@gmail.com>
2022-02-06 22:31:49 +0000

    Create an EditTodo page

--- src/components/pages/EditTodo.tsx
+++ src/components/pages/EditTodo.tsx
@@ -1,16 +1,37 @@
 import { FC } from "react";
+import { RootStateOrAny, useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 import Back from "../Back";
 import Content from "../Content";
 import Footer from "../Footer";
+import TodoForm from "../TodoForm";
+
+const Container = styled.div`
+  padding: 12px;
+
+  @media only screen and (min-width: 480px) {
+    padding: 24px;
+  }
+
+  @media only screen and (min-width: 768px) {
+    padding: 32px;
+  }
+`;
 
 const EditTodo: FC = () => {
-  const params = useParams();
+  const { todoId } = useParams();
+
+  const todo = useSelector((state: RootStateOrAny) => {
+    const currentTodoId = todoId !== undefined ? parseInt(todoId) : 0;
+    return state.todos.find((todo: Todo) => todo.id === currentTodoId);
+  });
 
   return (
-    <>
+    <Container>
       <Back />
-    </>
+      <TodoForm todo={todo} />
+    </Container>
   );
 };
 
