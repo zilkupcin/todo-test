9e2cd0dbe6b133905334071178692deeb668bd9d
zilkupcin <zil.kupcin@gmail.com>
2022-02-06 22:32:17 +0000

    Map through todos

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,13 +2,33 @@ import { FC } from "react";
 import styled from "styled-components";
 import TodoItem from "./TodoItem";
 
-const TodoList: FC = () => {
+interface Props {
+  todos: Array<Todo>;
+  onRemoveClick: (todo: Todo) => void;
+  onCompleteClick: (todo: Todo) => void;
+}
+
+const StyledTodoList = styled.ul`
+  @media only screen and (min-width: 768px) {
+    overflow-y: auto;
+    max-height: 500px;
+  }
+`;
+
+const TodoList: FC<Props> = ({ todos, onRemoveClick, onCompleteClick }) => {
   return (
-    <ul>
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-    </ul>
+    <StyledTodoList>
+      {todos.map((todo: Todo, index: number) => {
+        return (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onRemoveClick={onRemoveClick}
+            onCompleteClick={onCompleteClick}
+          />
+        );
+      })}
+    </StyledTodoList>
   );
 };
 
