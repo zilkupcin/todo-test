7ae96d7814c119a5c698a2f82eb112a4c98eb25a
zilkupcin <zil.kupcin@gmail.com>
2022-02-06 22:34:31 +0000

    Wrap the app in a store provider

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { store } from "./state/store";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById("todo")
